f0e959a4c7ca668bbbd8ec0df49d5a2cd430aa9e
Taehee Lee <kairess87@gmail.com>
2018-10-08 17:43:04 +0900

    draw overlay to face

--- main.py
+++ main.py
@@ -7,6 +7,33 @@ detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
 cap = cv2.VideoCapture('samples/girl.mp4')
+overlay = cv2.imread('samples/circle.png', cv2.IMREAD_UNCHANGED)
+
+def overlay_transparent(background_img, img_to_overlay_t, x, y, overlay_size=None):
+  bg_img = background_img.copy()
+  # convert 3 channels to 4 channels
+  if bg_img.shape[2] == 3:
+    bg_img = cv2.cvtColor(bg_img, cv2.COLOR_BGR2BGRA)
+
+  if overlay_size is not None:
+    img_to_overlay_t = cv2.resize(img_to_overlay_t.copy(), overlay_size)
+
+  b, g, r, a = cv2.split(img_to_overlay_t)
+
+  mask = cv2.medianBlur(a, 5)
+
+  h, w, _ = img_to_overlay_t.shape
+  roi = bg_img[int(y-h/2):int(y+h/2), int(x-w/2):int(x+w/2)]
+
+  img1_bg = cv2.bitwise_and(roi.copy(), roi.copy(), mask=cv2.bitwise_not(mask))
+  img2_fg = cv2.bitwise_and(img_to_overlay_t, img_to_overlay_t, mask=mask)
+
+  bg_img[int(y-h/2):int(y+h/2), int(x-w/2):int(x+w/2)] = cv2.add(img1_bg, img2_fg)
+
+  # convert 4 channels to 4 channels
+  bg_img = cv2.cvtColor(bg_img, cv2.COLOR_BGRA2BGR)
+
+  return bg_img
 
 while True:
   ret, img = cap.read()
@@ -20,7 +47,6 @@ while True:
   if len(faces) == 0:
     print('no faces!')
 
-  shapes2D = []
   for face in faces:
     dlib_shape = predictor(img, face)
     
@@ -28,6 +54,21 @@ while True:
 
     for s in shape_2d:
       cv2.circle(img, tuple(s), 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+    center_x, center_y = np.mean(shape_2d, axis=0).astype(np.int)
+
+    min_coords = np.min(shape_2d, axis=0)
+    max_coords = np.max(shape_2d, axis=0)
+
+    # draw min, max coords
+    cv2.circle(img, tuple(min_coords), 1, (255, 0, 0), 2, cv2.LINE_AA)
+    cv2.circle(img, tuple(max_coords), 1, (255, 0, 0), 2, cv2.LINE_AA)
+
+    # compute face size
+    face_size = max(max_coords - min_coords)
+
+    # draw overlay on face
+    img = overlay_transparent(img, overlay, center_x, center_y, overlay_size=(face_size, face_size))
 
   cv2.imshow('img', img)
   if cv2.waitKey(1) == ord('q'):
